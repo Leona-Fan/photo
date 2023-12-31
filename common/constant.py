3b799ed94f0c1cdd4f67bffe0a93d9b3f426e4e4
RevelWithFever <466721185@qq.com>
2023-12-31 12:48:42 +0800

    2023年12月31日12:47:41 完成手势1拍照 手势2切换风格 手势3切换随机参考照片 实现快捷键 q暂停 s重启 部分常量定义在constant中

--- common/constant.py
+++ common/constant.py
@@ -3,4 +3,9 @@
 # @Time     : 2023-12-30 13:06
 # @Function :
 
-STYLE = ['优雅','古风','搞怪','潮流','甜美']
+STYLE = ['优雅', '古风', '搞怪', '潮流', '甜美']
+# 画面刷新周期为 30ms
+REFRESH_PERIOD = 30
+# sampleCount = 5代表150ms 识别一次
+SAMPLE_COUNT = 5
+
